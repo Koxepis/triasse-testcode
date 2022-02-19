6062daa096057ba0dc743a53fb5f38637a0377fb
Shar51 <andiasar11@gmail.com>
2022-02-19 09:30:35 +0800

    initial config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import PageWithLayoutType from '../shared/types/pageWithLayout'
+import User from '../components/layouts/user'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type AppLayoutProps = {
+  Component: PageWithLayoutType
+  pageProps: any
+}
+
+function MyApp({ Component, pageProps }: AppLayoutProps) {
+  const Layout = Component.layout || User
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
